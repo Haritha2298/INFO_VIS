84e14a3e1ba2cfc3a3f4164043c53e23839af50b
anu43 <56151687+anu43@users.noreply.github.com>
2020-03-02 10:53:04 +0100

    auto-indentations and auto-corrections

--- Project_Final_Flask/run.py
+++ Project_Final_Flask/run.py
@@ -1,23 +1,23 @@
 from flask import Flask, render_template, url_for, request
-#from app import app
+# from app import app
 import data
 
 app = Flask(__name__)
 
-##@app.route('/')
-@app.route("/index", methods = ["GET","POST"])
+# @app.route('/')
+@app.route("/index", methods=["GET", "POST"])
 def index():
-    objects = ['Bridges','Buildings','Bus Stops','High Voltage Pylons','Lamp Posts','Metro Stops',
-               'Parks','Playgrounds','Sports Field','Toilets','Traffic Lights'
-    ,'Train Stations','Tram Stops','Trash','Trees','Wind Turbines']
+    objects = ['Bridges', 'Buildings', 'Bus Stops', 'High Voltage Pylons',
+               'Lamp Posts', 'Metro Stops', 'Parks', 'Playgrounds',
+               'Sports Field', 'Toilets', 'Traffic Lights', 'Train Stations',
+               'Tram Stops', 'Trash', 'Trees', 'Wind Turbines']
     if request.method == "POST":
-        selected_object = request.form.get('kailai')
+        selected_object = request.args.get('kailai')
         print("connection succesfull")
         print(selected_object)
-        #print("comes to call data")
         data.read_data(selected_object)
-        #print("exits call")
-    return render_template('index.html',objects = objects)
+    return render_template('index.html', objects=objects)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     app.run(debug=True)
